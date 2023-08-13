eaada4f75c40d9d0d55a02ca79b8f10d0738e168
arbishghori <128428077+arbishghori@users.noreply.github.com>
2023-08-13 05:40:22 -0700

    Add files via upload

---
+++ git 10/script.js
@@ -0,0 +1,7 @@
+ var str = num.toString();
+ if (str.charAt(str.length - 1) === "5") {
+ str = str.slice(0, str.length - 1) + "6";
+ }
+ num = Number(str);
+ prettyNum = num.toFixed(2);
+ console.log(2);
